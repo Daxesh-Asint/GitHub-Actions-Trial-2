e3c92e44a3630239ed8c1f209d09602edcbe6883
Daxesh-Asint <daxesh.prajapati@asint.net>
2026-09-22 12:58:48 +0530

    feat(sap): whitelist all DC and DC AddIn SAP CI/CD jobs and configure branch mappings

--- .github/scripts/notify_sap_event.sh
+++ .github/scripts/notify_sap_event.sh
@@ -31,6 +31,35 @@ ALLOWED_JOBS=(
   "Indorama-QA-234"
   "VMOS"
   "AsInt-AIS-02-DC"
+  "AsInt-AIS-02-DC-AddIn"
+  "AsInt-APM-01-DC"
+  "AsInt-APM-01-DC-AddIn"
+  "AsInt-APM-02-DC"
+  "AsInt-APM-02-DC-AddIn"
+  "AsInt-APM-EIOT-DC"
+  "AsInt-APM-EIOT-DC-AddIn"
+  "AsInt-DEMO-DC"
+  "AsInt-DEMO-DC-AddIn"
+  "AsInt-ST-DC"
+  "AsInt-ST-DC-AddIn"
+  "Baystar-DC"
+  "Baystar-DC-AddIn"
+  "Hemlock-NON-PROD-DC"
+  "Hemlock-NON-PROD-DC-AddIn"
+  "Hemlock-PROD-DC"
+  "Hemlock-PROD-DC-AddIn"
+  "Indorama-PROD-900-DC"
+  "Indorama-PROD-900-DC-AddIn"
+  "Indorama-PROD-933-DC"
+  "Indorama-PROD-933-DC-AddIn"
+  "Indorama-QA-233-DC"
+  "Indorama-QA-233-DC-AddIn"
+  "Indorama-QA-234-DC"
+  "Indorama-QA-234-DC-AddIn"
+  "IRC-DC"
+  "IRC-DC-AddIn"
+  "VMOS-DC"
+  "VMOS-DC-AddIn"
 )
 
 IS_ALLOWED=false
@@ -112,6 +141,93 @@ case "$RESOURCE_NAME" in
     ;;
   "AsInt-AIS-02-DC")
     TARGET_BRANCH="tenant/asint-ais-02-dc"
+    ;;
+  "AsInt-AIS-02-DC-AddIn")
+    TARGET_BRANCH="tenant/asint-ais-02-dc-addin"
+    ;;
+  "AsInt-APM-01-DC")
+    TARGET_BRANCH="tenant/asint-apm-01-dc"
+    ;;
+  "AsInt-APM-01-DC-AddIn")
+    TARGET_BRANCH="tenant/asint-apm-01-dc-addin"
+    ;;
+  "AsInt-APM-02-DC")
+    TARGET_BRANCH="tenant/asint-apm-02-dc"
+    ;;
+  "AsInt-APM-02-DC-AddIn")
+    TARGET_BRANCH="tenant/asint-apm-02-dc-addin"
+    ;;
+  "AsInt-APM-EIOT-DC")
+    TARGET_BRANCH="tenant/asint-apm-eiot-dc"
+    ;;
+  "AsInt-APM-EIOT-DC-AddIn")
+    TARGET_BRANCH="tenant/asint-apm-eiot-dc-addin"
+    ;;
+  "AsInt-DEMO-DC")
+    TARGET_BRANCH="tenant/asint-demo-dc"
+    ;;
+  "AsInt-DEMO-DC-AddIn")
+    TARGET_BRANCH="tenant/asint-demo-dc-addin"
+    ;;
+  "AsInt-ST-DC")
+    TARGET_BRANCH="tenant/asint-st-env-dc"
+    ;;
+  "AsInt-ST-DC-AddIn")
+    TARGET_BRANCH="tenant/asint-st-env-dc-addin"
+    ;;
+  "Baystar-DC")
+    TARGET_BRANCH="tenant/baystar-dc"
+    ;;
+  "Baystar-DC-AddIn")
+    TARGET_BRANCH="tenant/baystar-dc-addin"
+    ;;
+  "Hemlock-NON-PROD-DC")
+    TARGET_BRANCH="tenant/hemlock-non-prod-dc"
+    ;;
+  "Hemlock-NON-PROD-DC-AddIn")
+    TARGET_BRANCH="tenant/hemlock-non-prod-dc-addin"
+    ;;
+  "Hemlock-PROD-DC")
+    TARGET_BRANCH="tenant/hemlock-prod-dc"
+    ;;
+  "Hemlock-PROD-DC-AddIn")
+    TARGET_BRANCH="tenant/hemlock-prod-dc-addin"
+    ;;
+  "Indorama-PROD-900-DC")
+    TARGET_BRANCH="tenant/indorama-prod-900-dc"
+    ;;
+  "Indorama-PROD-900-DC-AddIn")
+    TARGET_BRANCH="tenant/indorama-prod-900-dc-addin"
+    ;;
+  "Indorama-PROD-933-DC")
+    TARGET_BRANCH="tenant/indorama-prod-933-dc"
+    ;;
+  "Indorama-PROD-933-DC-AddIn")
+    TARGET_BRANCH="tenant/indorama-prod-933-dc-addin"
+    ;;
+  "Indorama-QA-233-DC")
+    TARGET_BRANCH="tenant/indorama-qa-233-dc"
+    ;;
+  "Indorama-QA-233-DC-AddIn")
+    TARGET_BRANCH="tenant/indorama-qa-233-dc-addin"
+    ;;
+  "Indorama-QA-234-DC")
+    TARGET_BRANCH="tenant/indorama-qa-234-dc"
+    ;;
+  "Indorama-QA-234-DC-AddIn")
+    TARGET_BRANCH="tenant/indorama-qa-234-dc-addin"
+    ;;
+  "IRC-DC")
+    TARGET_BRANCH="tenant/irc-dc"
+    ;;
+  "IRC-DC-AddIn")
+    TARGET_BRANCH="tenant/irc-dc-addin"
+    ;;
+  "VMOS-DC")
+    TARGET_BRANCH="tenant/vmos-dc"
+    ;;
+  "VMOS-DC-AddIn")
+    TARGET_BRANCH="tenant/vmos-dc-addin"
     ;;
 esac
 
